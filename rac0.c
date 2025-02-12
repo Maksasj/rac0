49ab58fcbe7fe9b2e4067e1a9ab01cc4aaf5ca45
Maksim Jaroslavcevas <radioboos@gmail.com>
2025-02-12 19:46:51 +0200

    experimental device handling

--- rac0.c
+++ rac0.c
@@ -3,9 +3,18 @@
 
 #include "rac0.h"
 
+void debug_console_device_push(void* device_data, rac0_u64_t adress, rac0_value_t value) {
+    PLUM_LOG(PLUM_EXPERIMENTAL, "%d", value);
+}
+
+rac0_value_t debug_console_device_pool(void* device_data, rac0_u64_t adress) {
+    return 0;
+}
+
 int main() {
     rac0_cpu_t cpu = (rac0_cpu_t) {
         .pc = 0,
+        .device = 0,
         .halted = 0,
         .stack = {
             .top = 0 
@@ -15,19 +24,28 @@ int main() {
     rac0_inst_t insts[] = {
         (rac0_inst_t) { .opcode = RAC0_PUSHA_OPCODE, .value = 0x0000000000000001 },
         (rac0_inst_t) { .opcode = RAC0_ADDAT_OPCODE, .value = 1 },
-        (rac0_inst_t) { .opcode = RAC0_JMPLUA_OPCODE, .value = 1 * sizeof(rac0_inst_t) },
+        (rac0_inst_t) { .opcode = RAC0_PUSHDA_OPCODE, .value = 0 },
+        (rac0_inst_t) { .opcode = RAC0_JMPLUA_OPCODE, .value = 2 * sizeof(rac0_inst_t) },
         (rac0_inst_t) { .opcode = RAC0_HALT_OPCODE },
     };
 
-    rac0_memory_t ram = (rac0_memory_t) {
+    rac0_memory_t memory = (rac0_memory_t) {
         // .memory = (rac0_byte_t*) malloc(sizeof(rac0_byte_t) * RAC0_MEGABYTE_SIZE)  
         .memory = (rac0_byte_t*) &insts
+    };
+
+    rac0_device_t devices[] = {
+        (rac0_device_t) { 
+            .device_data = NULL, 
+            .push = debug_console_device_push, 
+            .pool = debug_console_device_pool 
+        }
     };
 
     PLUM_LOG(PLUM_INFO, "CPU started");
 
     while(!cpu.halted)
-        rac0_cpu_inst_cycle(&cpu, &ram);
+        rac0_cpu_inst_cycle(&cpu, &memory, devices);
 
     PLUM_LOG(PLUM_INFO, "CPU halted");
 
--- rac0.h
+++ rac0.h
@@ -36,16 +36,28 @@ typedef rac0_u16_t rac0_opcode_t;
 #define RAC0_DROP_OPCODE        ((rac0_opcode_t) 5)
 #define RAC0_SWAP_OPCODE        ((rac0_opcode_t) 6)
 
-#define RAC0_ADD_OPCODE         ((rac0_opcode_t) 7)
-#define RAC0_ADDC_OPCODE        ((rac0_opcode_t) 8)
-#define RAC0_ADDAT_OPCODE       ((rac0_opcode_t) 9)
-#define RAC0_ADDAN_OPCODE       ((rac0_opcode_t) 10)
+#define RAC0_STORE_OPCODE        ((rac0_opcode_t) 7)
+#define RAC0_STOREA_OPCODE       ((rac0_opcode_t) 8)
+#define RAC0_LOAD_OPCODE         ((rac0_opcode_t) 9)
+#define RAC0_LOADA_OPCODE        ((rac0_opcode_t) 10)
 
-#define RAC0_JMPGA_OPCODE       ((rac0_opcode_t) 11)
-#define RAC0_JMPLUA_OPCODE       ((rac0_opcode_t) 12)
-#define RAC0_JMPLDA_OPCODE       ((rac0_opcode_t) 13)
+#define RAC0_ADD_OPCODE         ((rac0_opcode_t) 11)
+#define RAC0_ADDC_OPCODE        ((rac0_opcode_t) 12)
+#define RAC0_ADDAT_OPCODE       ((rac0_opcode_t) 13)
+#define RAC0_ADDAN_OPCODE       ((rac0_opcode_t) 14)
 
-#define RAC0_WAIT_OPCODE        ((rac0_opcode_t) 14)
+#define RAC0_JMPGA_OPCODE       ((rac0_opcode_t) 15)
+#define RAC0_JMPLUA_OPCODE      ((rac0_opcode_t) 16)
+#define RAC0_JMPLDA_OPCODE      ((rac0_opcode_t) 17)
+
+#define RAC0_SETDA_OPCODE       ((rac0_opcode_t) 18)
+#define RAC0_SETDT_OPCODE       ((rac0_opcode_t) 19)
+#define RAC0_POOLDA_OPCODE      ((rac0_opcode_t) 20)
+#define RAC0_POOLDT_OPCODE      ((rac0_opcode_t) 21)
+#define RAC0_PUSHDA_OPCODE      ((rac0_opcode_t) 22)
+#define RAC0_PUSHDT_OPCODE      ((rac0_opcode_t) 23)
+
+#define RAC0_WAIT_OPCODE        ((rac0_opcode_t) 24)
 
 static char* OPCODE_STRING[] = {
     "HALT",
@@ -57,6 +69,11 @@ static char* OPCODE_STRING[] = {
     "DROP",
     "SWAP",
 
+    "STORE",
+    "STOREA",
+    "LOAD",
+    "LOADA",
+
     "ADD",
     "ADDC",
     "ADDAT",
@@ -66,8 +83,21 @@ static char* OPCODE_STRING[] = {
     "JMPLUA",
     "JMPLDA",
 
+    "SETDA",
+    "SETDT",
+    "POOLDA",
+    "POOLDT",
+    "PUSHDA",
+    "PUSHDT",
+
     "WAIT"
 };
+
+typedef struct {
+    void* device_data;
+    void (*push)(void* device_data, rac0_u64_t adress, rac0_value_t value);
+    rac0_value_t (*pool)(void* device_data, rac0_u64_t adress);
+} rac0_device_t;
 
 typedef struct {
     rac0_opcode_t opcode;   // 16 bits | 2 bytes
@@ -114,7 +144,8 @@ void rac0_stack_drop(rac0_stack_t* stack) {
 
 typedef struct {
     rac0_stack_t stack;
-    rac0_u64_t pc; 
+    rac0_u64_t pc;
+    rac0_u64_t device; 
     rac0_byte_t halted;
 } rac0_cpu_t;
 
@@ -123,13 +154,13 @@ typedef struct {
 } rac0_memory_t;
 
 rac0_inst_t rac0_fetch_inst(rac0_u64_t pc, rac0_memory_t* memory);
-void rac0_cpu_inst_cycle(rac0_cpu_t* cpu, rac0_memory_t* memory);
+void rac0_cpu_inst_cycle(rac0_cpu_t* cpu, rac0_memory_t* memory, rac0_device_t* devices);
 
 rac0_inst_t rac0_fetch_inst(rac0_u64_t pc, rac0_memory_t* memory) {
     return *((rac0_inst_t*) &memory->memory[pc]);
 }
 
-void rac0_cpu_inst_cycle(rac0_cpu_t* cpu, rac0_memory_t* memory) {
+void rac0_cpu_inst_cycle(rac0_cpu_t* cpu, rac0_memory_t* memory, rac0_device_t* devices) {
     rac0_inst_t inst = rac0_fetch_inst(cpu->pc, memory);
 
     if(inst.opcode == RAC0_HALT_OPCODE) {
@@ -151,13 +182,21 @@ void rac0_cpu_inst_cycle(rac0_cpu_t* cpu, rac0_memory_t* memory) {
         rac0_stack_drop(&cpu->stack);
         goto inc;
     } else if(inst.opcode == RAC0_SWAP_OPCODE) {
-        // todo implement
+        PLUM_LOG(PLUM_ERROR, "Opcode SWAP is not implemented");
     } else if(inst.opcode == RAC0_ADD_OPCODE) {
         rac0_value_t top = rac0_stack_get_top(&cpu->stack);
         rac0_value_t next = rac0_stack_get_next(&cpu->stack);
 
         rac0_stack_push(&cpu->stack, top + next);
         goto inc;
+    } else if(inst.opcode == RAC0_STORE_OPCODE) {
+        PLUM_LOG(PLUM_ERROR, "Opcode STORE is not implemented");
+    } else if(inst.opcode == RAC0_STOREA_OPCODE) {
+        PLUM_LOG(PLUM_ERROR, "Opcode STOREA is not implemented");
+    } else if(inst.opcode == RAC0_LOAD_OPCODE) {
+        PLUM_LOG(PLUM_ERROR, "Opcode LOAD is not implemented");
+    } else if(inst.opcode == RAC0_LOADA_OPCODE) {
+        PLUM_LOG(PLUM_ERROR, "Opcode LOADA is not implemented");
     } else if(inst.opcode == RAC0_ADDC_OPCODE) {
         rac0_value_t top = rac0_stack_get_top(&cpu->stack);
         rac0_value_t next = rac0_stack_get_next(&cpu->stack);
@@ -196,6 +235,22 @@ void rac0_cpu_inst_cycle(rac0_cpu_t* cpu, rac0_memory_t* memory) {
     } else if(inst.opcode == RAC0_JMPLDA_OPCODE) {
         cpu->pc += inst.value;
         goto cont;
+    } else if(inst.opcode == RAC0_SETDA_OPCODE) {
+        cpu->device = inst.value;
+        goto inc;
+    } else if(inst.opcode == RAC0_SETDT_OPCODE) {
+        PLUM_LOG(PLUM_ERROR, "Opcode SETDT is not implemented");
+    } else if(inst.opcode == RAC0_POOLDA_OPCODE) {
+        PLUM_LOG(PLUM_ERROR, "Opcode POOLDA is not implemented");
+    } else if(inst.opcode == RAC0_POOLDT_OPCODE) {
+        PLUM_LOG(PLUM_ERROR, "Opcode POOLDT is not implemented");
+    } else if(inst.opcode == RAC0_PUSHDA_OPCODE) {
+        rac0_value_t top = rac0_stack_get_top(&cpu->stack);
+        rac0_device_t device = devices[cpu->device];
+        (*device.push)(device.device_data, inst.value, top);
+        goto inc;
+    } else if(inst.opcode == RAC0_PUSHDT_OPCODE) {
+        PLUM_LOG(PLUM_ERROR, "Opcode PUSHDT is not implemented");
     } else if(inst.opcode == RAC0_WAIT_OPCODE) {
         goto inc; // do nothing
     } else {
